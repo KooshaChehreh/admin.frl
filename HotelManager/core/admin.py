c956c9eccfdc94634f60196eac233ad2a758d1bc
markdown <mehdimirzaie1378@gmail.com>
2023-05-23 10:55:03 +0330

    HotelManager | core | Create BaseModel, BaseManager, BaseAdmin

---
+++ HotelManager/core/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from django.utils import timezone
+
+
+class BaseAdmin(admin.ModelAdmin):
+    def logical_delete(self, request, queryset):
+        queryset.update(deleted_at=timezone.now())
+        queryset.update(is_deleted=True)
+        queryset.update(is_active=False)
+
+    def logical_restore(self, request, queryset):
+        queryset.update(restored_at=timezone.now())
+        queryset.update(is_deleted=False)
+        queryset.update(is_active=True)
+
+    def deactivate(self, request, queryset):
+        queryset.update(is_active=False)
+
+    def activate(self, request, queryset):
+        queryset.update(is_active=True)
+
+    actions = ['logical_delete', 'logical_restore', 'deactivate', 'activate']
---
+++ HotelManager/core/managers.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class BaseManager(models.Manager):
+
+    def get_active_list(self):
+        return super().get_queryset().filter(is_deleted=False, is_active=True)
+
+    def get_deleted_list(self):
+        return super().get_queryset().filter(is_deleted=True)
+
+    def get_deactivate_list(self):
+        return super().get_queryset().filter(is_active=False)
