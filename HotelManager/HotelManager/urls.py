c1b302516a70499ee996b703ae85037580dde3d0
Aryan Hamedani <aryn.hmd@gmail.com>
2023-05-25 14:54:12 +0330

    Adding dashboard

--- HotelManager/HotelManager/urls.py
+++ HotelManager/HotelManager/urls.py
@@ -18,6 +18,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("jet/", include("jet.urls", "jet")),  # Django JET URLS
+    path(r"^jet/", include("jet.urls", "jet")),  # Django JET URLS
+    path(
+        r"^jet/dashboard/", include("jet.dashboard.urls", "jet-dashboard")
+    ),  # Django JET dashboard URLS
     path("admin/", admin.site.urls),
 ]
