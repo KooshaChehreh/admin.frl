00fe1af56ed388513e2bf58662738ff99439cf49
markdown <mehdimirzaie1378@gmail.com>
2023-05-31 13:12:50 +0330

    HotelManager | Fixed docker compose

--- HotelManager/HotelManager/settings.py
+++ HotelManager/HotelManager/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-+bz7ij1t78v3irhu+k+)rnf(o8(+&2l5a%y#*sao+o1=4u_evn"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -125,7 +125,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
