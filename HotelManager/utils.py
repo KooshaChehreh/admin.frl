2b6d1fbc2ec5c96b756bcfdad3ed6961dd272d34
markdown <mehdimirzaie1378@gmail.com>
2023-06-04 11:55:24 +0330

    HotelManager | utils.py | Removed send_otp_code

--- HotelManager/utils.py
+++ HotelManager/utils.py
@@ -2,23 +2,6 @@ import calendar
 from datetime import datetime
 
 from django.contrib.auth.mixins import UserPassesTestMixin
-from kavenegar import *
-
-
-def send_otp_code(phone_number, code):
-    try:
-        api = KavenegarAPI('API_KEY')
-        params = {
-            'sender': '',
-            'receptor': phone_number,
-            'message': f'{code} کد تایید شما '
-        }
-        response = api.sms_send(params)
-        print(response)
-    except APIException as e:
-        print(e)
-    except HTTPException as e:
-        print(e)
 
 
 class IsAdminUserMixin(UserPassesTestMixin):
